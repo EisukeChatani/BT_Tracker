d55d84d8ffb784c662584b0e3bd577b375ec0d71
Eisuke Chatani <2020m065@gmail.com>
2020-06-12 12:32:54 +0900

    added URL

--- app/src/main/java/com/example/bttracker/MainActivity.java
+++ app/src/main/java/com/example/bttracker/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.bttracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToLogActivity(View view) {
+        Intent toLog = new Intent(this,LogActivity.class);
+        startActivity(toLog);
+    }
+
+    public void goToNormalActivity(View view) {
+        Intent toNormal = new Intent(this, NormalActivity.class);
+        startActivity(toNormal);
+    }
+
+    public void goToMechanismActivity(View view) {
+        Intent toMechanism = new Intent( this, MechanismActivity.class);
+        startActivity(toMechanism);
+    }
+
+    public void openHandlingFeverWebsite(View view) {
+        Intent tolink = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.healthline.com/health/how-to-break-a-fever"));
+
+        startActivity(tolink);
     }
 }
